7597997b0dc46b06c5923c720633363619a12e04
PatelYagna <86132344+PatelYagna@users.noreply.github.com>
2023-09-26 23:17:14 -0500

    Update functions.h

--- functions.h
+++ functions.h
@@ -43,3 +43,54 @@ vector<string> textWordVec(string text);
 // sentimentVector and returns the overall sentiment value.
 float sentCalc(vector<string> wordVector,
                vector<sentimentFile> sentimentVector);
+
+// Pre: sentimentVector is a vector of sentimentFile structs that contains words
+// and their corresponding sentiment values, startIndex is an integer that
+// represents the starting index in the sentimentVector to search for the
+// searchWord, endIndex is an integer that represents the ending index in the
+// sentimentVector to search for the searchWord, searchWord is a string
+// representing the word to search for in the sentimentVector. Post: Calculates
+// the sentiment value for searchWord using the sentiment values of words in
+// sentimentVector between startIndex and endIndex and returns a sentimentFile
+// struct containing searchWord and its calculated sentiment value.
+sentimentFile theCalc(vector<sentimentFile> sentimentVector, int startIndex,
+                      int endIndex, string searchWord);
+
+// Pre: sentimentVector is a vector of sentimentFile structs that contains words
+// and their corresponding sentiment values. Post: Prints the words and
+// sentiment values in sentimentVector to the console.
+void printSentimentVector(vector<sentimentFile> sentimentVector);
+
+// Pre: sentimentVector is a vector of sentimentFile structs that contains words
+// and their corresponding sentiment values, negVec is a vector of sentimentFile
+// structs that contains negative words and their corresponding sentiment
+// values, wordVector is a vector of strings that contains words to be analyzed,
+// text is a string that contains the text to be analyzed. Post: Replaces
+// negative words in wordVector with their positive counterparts using negVec
+// and updates the text with the replaced words.
+void negativeConversion(vector<sentimentFile> sentimentVector,
+                        vector<sentimentFile> negVec,
+                        vector<string> &wordVector, string &text);
+
+// Pre: sentimentVector is a vector of sentimentFile structs that contains words
+// and their corresponding sentiment values, posVec is a vector of sentimentFile
+// structs that contains positive words and their corresponding sentiment
+// values, wordVector is a vector of strings that contains words to be analyzed,
+// text is a string that contains the text to be analyzed. Post: Replaces
+// positive words in wordVector
+void positiveConversion(vector<sentimentFile> sentimentVector,
+                        vector<sentimentFile> posVec,
+                        vector<string> &wordVector, string &text);
+
+// PRE: Takes in a reference to a string 'input' and a string 'text'
+// POST: Outputs the contents of the string 'text' to a file with the name
+// specified in 'input'
+void outputFile(string &input, string text);
+
+// PRE: Takes in a reference to a string 'text'
+// POST: Prints the contents of the string 'text' to the console
+void printText(string &text);
+
+// PRE: Takes in a string 'word'
+// POST: Returns a lowercase version of the input string 'word'
+string lower(string word);
