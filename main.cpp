0e7496a70f8c757f170c302fe26ccf4ce039f3f9
PatelYagna <86132344+PatelYagna@users.noreply.github.com>
2023-09-26 23:16:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,5 +32,59 @@ int main() {
     cout << "Q - to Quit" << endl << endl;
     cout << "Your choice: ";
     cin >> choice;
+
+    switch (toupper(choice)) {
+    case 'P':
+      cout << endl
+           << "----------------------------------------------------------------"
+              "------------------------------------------"
+           << endl
+           << endl;
+      cout << endl
+           << "Okay! I will edit file: " << input << " to be more positive!"
+           << endl
+           << endl;
+      positiveConversion(sentiment, positive, wordVector, text);
+      cout << "After this chage, the new file is: " << endl;
+      printText(text);
+      cout << endl << endl;
+      cout << "New Sentiment Value = " << sentCalc(wordVector, sentiment)
+           << endl
+           << endl;
+      outputFile(input, text);
+      cout << "The new output file will be stored in: " << input << endl
+           << endl;
+      cout << "Would you like to go again? ([Y]es or [N]o) ";
+      cin >> loop;
+      break;
+
+    case 'N':
+      cout << endl
+           << "----------------------------------------------------------------"
+              "------------------------------------------"
+           << endl
+           << endl;
+      cout << endl
+           << "Okay! I will edit file: " << input << " to be more negative!"
+           << endl
+           << endl;
+      negativeConversion(sentiment, negative, wordVector, text);
+      cout << endl << "After this chage, the new file is: " << endl;
+      printText(text);
+      cout << endl << endl;
+      cout << "New Sentiment Value = " << sentCalc(wordVector, sentiment)
+           << endl
+           << endl;
+      outputFile(input, text);
+      cout << "The new output file will be stored in: " << input << endl
+           << endl;
+      cout << "Would you like to go again? (Y/N) ";
+      cin >> loop;
+      break;
+
+    case 'Q':
+      loop = 'N';
+      break;
+    }
   }
 }
